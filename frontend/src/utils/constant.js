1c8755bd99ac252accdb4cebb3c5cfe27a3f72c1
Shailendra Singh Rawat <rwtshail1@gmail.com>
2025-07-08 14:57:35 +0530

    constants updated

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,15 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+/* export const USER_API_END_POINT = 'http://localhost:8000/api/v1/user';
+export const JOB_API_END_POINT = 'http://localhost:8000/api/v1/job';
+export const APPLICATION_API_END_POINT =
+  'http://localhost:8000/api/v1/application';
+export const COMPANY_API_END_POINT = 'http://localhost:8000/api/v1/company';
+ */
+
+export const USER_API_END_POINT =
+  'https://nexthire-t9l5.onrender.com/api/v1/user';
+export const JOB_API_END_POINT =
+  'https://nexthire-t9l5.onrender.com/api/v1/job';
+export const APPLICATION_API_END_POINT =
+  'https://nexthire-t9l5.onrender.com/api/v1/application';
+export const COMPANY_API_END_POINT =
+  'https://nexthire-t9l5.onrender.com/api/v1/company';
